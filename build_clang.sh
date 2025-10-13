568c146de7d4c12dc7ac84cbf20218d50e71a4e4
李小东 <825346670@qq.com>
2025-10-13 18:44:11 +0800

    Add files via upload

--- build_clang.sh
+++ build_clang.sh
@@ -1,9 +1,10 @@
 export PATH="/lib/llvm-20/bin:$PATH"
 
-chmod 777 clang-wrapper.sh
-
-make install CC="$PWD/clang-wrapper.sh" CXX="$PWD/clang-wrapper.sh" -j$(nproc)
-
+make install CC=clang -Qunused-arguments -Wno-unknown-warning-option CXX=clang++ -Qunused-arguments -Wno-unknown-warning-option -j$(nproc)
 mv -f config.mak.bak config.mak
+make install CC=clang -Qunused-arguments -Wno-unknown-warning-option CXX=clang++ -Qunused-arguments -Wno-unknown-warning-option -j$(nproc)
 
-make install CC="$PWD/clang-wrapper.sh" CXX="$PWD/clang-wrapper.sh" -j$(nproc)
+#chmod 777 clang-wrapper.sh
+#make install CC="$PWD/clang-wrapper.sh" CXX="$PWD/clang-wrapper.sh" -j$(nproc)
+#mv -f config.mak.bak config.mak
+#make install CC="$PWD/clang-wrapper.sh" CXX="$PWD/clang-wrapper.sh" -j$(nproc)
