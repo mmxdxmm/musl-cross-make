05e2dc8ee43276c6f398cd74c9b39b7bf034ee1c
李小东 <825346670@qq.com>
2025-10-14 19:23:21 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 make install -j$(nproc)
-
 mv -f config.mak.bak config.mak
-
 make install -j$(nproc)
+
+out_dir=$PWD
+cd /root
+zip -r -9 binutils.zip binutils
+cp binutils.zip $out_dir/binutils.zip
--- build_clang.sh
+++ build_clang.sh
@@ -8,3 +8,8 @@ make install CC="clang -Qunused-arguments -Wno-unknown-warning-option" CXX="clan
 #make install CC="$PWD/clang-wrapper.sh" CXX="$PWD/clang-wrapper.sh" -j$(nproc)
 #mv -f config.mak.bak config.mak
 #make install CC="$PWD/clang-wrapper.sh" CXX="$PWD/clang-wrapper.sh" -j$(nproc)
+
+out_dir=$PWD
+cd /root
+zip -r -9 binutils.zip binutils
+cp binutils.zip $out_dir/binutils.zip
