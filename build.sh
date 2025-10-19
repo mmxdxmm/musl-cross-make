7561dd1025f1be0820ea31d18a842f3f364bcbbe
李小东 <1658011573@qq.com>
2025-10-19 21:48:17 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-make install -j$(nproc)
+set_CC="cc -Os -ffunction-sections -fdata-sections -Wno-error -mimplicit-it=always"
+set_CXX="c++ -Os -ffunction-sections -fdata-sections -Wno-error -mimplicit-it=always"
+set_LD="ld --strip-debug --gc-sections"
+
+make install LD="$set_LD" HOSTLD="$set_LD" CC="$set_CC" CXX="$set_CXX" HOSTCC="$set_CC" HOSTCXX="$set_CXX" -j$(nproc)
 mv -f config.mak.bak config.mak
-make install -j$(nproc)
+make install LD="$set_LD" HOSTLD="$set_LD" CC="$set_CC" CXX="$set_CXX" HOSTCC="$set_CC" HOSTCXX="$set_CXX" -j$(nproc)
 
 out_dir=$PWD
 cd /root
