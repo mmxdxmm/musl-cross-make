94ed31361680810c0e34306b79eac1157580f40f
李小东 <1658011573@qq.com>
2025-10-19 23:48:51 +0800

    Add files via upload

--- build_with_clang.sh
+++ build_with_clang.sh
@@ -1,7 +1,7 @@
 export PATH="/lib/llvm-20/bin:$PATH"
 
-set_CC="ccache clang -Os -ffunction-sections -fdata-sections -Wno-error -Qunused-arguments -Wno-unknown-warning-option -stdlib=libc++"
-set_LD="ld.lld --strip-debug --gc-sections -stdlib=libc++ -lc++ -lc++abi"
+set_CC="ccache clang -Os -ffunction-sections -fdata-sections -Wno-error -Qunused-arguments -Wno-unknown-warning-option -stdlib=libstdc++"
+set_LD="ld.lld --strip-debug --gc-sections -stdlib=libstdc++ -lstdc++"
 set_other="AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump HOSTAR=llvm-ar"
 
 make install LD="$set_LD" HOSTLD="$set_LD" CC="$set_CC" CXX="$set_CC" HOSTCC="$set_CC" HOSTCXX="$set_CC" $set_other -j$(nproc)
